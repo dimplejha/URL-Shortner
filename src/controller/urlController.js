434fef05e1d7faefbc7288f0e52136ba9136384a
Manish-162403 <naamcool420@gmail.com>
2022-04-04 12:50:29 +0530

    minor changes

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -2,9 +2,24 @@ const urlModel = require("../models/urlModel")
 let axios = require("axios")
 
 
+const isValid = function(value){
+    if(typeof value ==undefined ||  value ==null)return false
+    if(typeof value==='string'&&value.trim().length===0) return false
+    return true
+}
+
 const createUrl = async function(req,res){
 try{
-let data = req.body
+
+    let data = req.body
+
+if(!isValid(data)){return res.status.send({status:false, message : "please provide data"})}
+
+if(!isValid(data.longUrl)){return res.status(400).send({status:false, message : "please provide longUrl"})}
+
+    if(!(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/.test(data.longUrl))){
+        return res.send(400).send({status:false, message : "Please input a valid url"})
+    }
 
 const savedData = await urlModel.create(data)
 
